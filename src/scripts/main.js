614f37fb3f9fc4aa5bdc58c746c754f48a3d877b
Evg3n22 <evgen.gulevaty@gmail.com>
2026-01-04 19:22:59 +0200

    add success and error handler for both promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,18 +15,22 @@ const promise2 = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-promise1.then((message) => {
+const showSuccess = (message) => {
   const messageElement = document.createElement('div');
 
   messageElement.classList.add('message');
   messageElement.textContent = message;
   document.body.appendChild(messageElement);
-});
+};
 
-promise2.catch((error) => {
+const showError = (error) => {
   const errorElement = document.createElement('div');
 
   errorElement.classList.add('message', 'error-message');
-  errorElement.textContent = error;
+  errorElement.textContent = error.message;
   document.body.appendChild(errorElement);
-});
+};
+
+promise1.then(showSuccess).catch(showError);
+
+promise2.then(showSuccess).catch(showError);
